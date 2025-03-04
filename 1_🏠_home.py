31ea6b20a1d6a362c2cc8e1ce943473a1986af0b
lucaskatata <104213451+lucaskatata@users.noreply.github.com>
2025-03-04 09:27:35 -0300

    Adicionando projeto

---
+++ 1_🏠_home.py
@@ -0,0 +1,41 @@
+# %%
+import streamlit as st
+import webbrowser
+import pandas as pd
+from datetime import datetime
+
+# %%
+if 'data' not in st.session_state:
+    df_data = pd.read_csv(r'D:\Lucas\Estudos - Python\Asimov Academy\Criando Aplicativos Web com Streamlit\Projeto FIFA\datasets\CLEAN_FIFA23_official_data.csv', index_col=0)
+    df_data = df_data[df_data['Contract Valid Until'] >= datetime.today().year]
+    df_data = df_data[df_data['Value(£)'] > 0]
+    df_data = df_data.sort_values(by='Overall', ascending=False)
+    st.session_state['data'] = df_data
+
+st.set_page_config(layout='wide',
+                   page_title='Home',
+                   page_icon='🏠')
+
+st.markdown('# FIFA 23 OFFICIAL DATASET! ⚽')
+st.sidebar.markdown('Desenvolvido por Lucas')
+
+btn = st.button('Acesse os dados do Kaggle!')
+if btn:
+    webbrowser.open_new_tab('https://www.kaggle.com/datasets/bryanb/fifa-player-stats-database')
+
+st.markdown(
+    """
+    O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+"""
+)
